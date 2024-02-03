9ad863d5d26f34f3a925482de20cf8bd629ecd7b
alexs <alexei.shef@gmail.com>
2024-02-03 13:31:03 +0300

    3 endpoint(rate on exact date from banks api)

---
+++ requests_to_api/belarusbank_rate_on_date.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from datetime import datetime
+
+# date = input('Date no later than 4 days, yyyy-mm-dd: ')
+input_date = '2024-02-02'
+url = 'https://belarusbank.by/api/kurs_cards'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    for date_time in data:
+        date_time_object = datetime.strptime(date_time['kurs_date_time'], '%Y-%m-%d %H:%M:%S')
+        date_only = date_time_object.strftime('%Y-%m-%d')
+        if date_only == input_date:
+            formatted_date_time = json.dumps(date_time, indent=2)
+            print(formatted_date_time)
+            break
+
+else:
+    print(f"Error {response.status_code}: {response.text}")
---
+++ requests_to_api/nbrb_rate_on_date.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://api.nbrb.by/exrates/rates[/{cur_id}]'
+headers = {'ondate': '2023-01-10'}
+
+response = requests.get(url, params=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error {response.status_code}: {response.text}")
