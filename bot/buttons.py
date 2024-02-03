a02827693736cf40ed534a5c75d935b565cab034
Evgeniy Grakovich <grakovich-e@yandex.by>
2024-02-03 11:24:06 +0300

    start message handler for bot

---
+++ bot/buttons.py
@@ -0,0 +1,3 @@
+national_bank_btn = 'Национальный банк'
+alfa_bank_btn = 'Альфа банк'
+belarus_bank_btn = 'Беларусбанк'
--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,3 +1,9 @@
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+import buttons
+
+choose_bank_kb = ReplyKeyboardMarkup([[buttons.national_bank_btn], [
+                                    buttons.alfa_bank_btn, buttons.belarus_bank_btn]],
+                                     resize_keyboard=True,
+                                     one_time_keyboard=True)
