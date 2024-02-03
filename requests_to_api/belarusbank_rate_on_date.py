41930c49572994025e00fbc191caab384f032173
alexs <alexei.shef@gmail.com>
2024-02-03 17:27:39 +0300

    3 endpoint fix. add answer if no data

--- requests_to_api/belarusbank_rate_on_date.py
+++ requests_to_api/belarusbank_rate_on_date.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 
 # date = input('Date no later than 4 days, yyyy-mm-dd: ')
-input_date = '2024-02-02'
+input_date = '2024-01-31'
 url = 'https://belarusbank.by/api/kurs_cards'
 
 response = requests.get(url)
@@ -17,6 +17,8 @@ if response.status_code == 200:
             formatted_date_time = json.dumps(date_time, indent=2)
             print(formatted_date_time)
             break
+    else:
+        print('There is no data for this date')
 
 else:
     print(f"Error {response.status_code}: {response.text}")
