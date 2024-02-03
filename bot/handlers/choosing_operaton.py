b97def33a83a4df8feeadfb96c7461e9053d4522
Evgeniy Grakovich <grakovich-e@yandex.by>
2024-02-03 12:30:36 +0300

    delete comments

--- bot/handlers/choosing_operaton.py
+++ bot/handlers/choosing_operaton.py
@@ -20,13 +20,3 @@ async def send_welcome(message: types.Message, state: FSMContext):
         pass
 
 
-    #         data = await state.get_data()
-    #         bank_name = data.get('chosen_bank')
-    #         await state.update_data(chosen_currency=user_input)
-    #         await message.answer(texts.generate_choose_operation(bank_name, user_input), 
-    #                             reply_markup=kb.choose_operation_kb)
-    #         await state.update_data(chosen_currency=user_input)
-    #         await State.choosing_operation.set()
-    # else:
-    #     await message.answer(texts.invalid_input, reply_markup=kb.choose_currency_kb)
-        
