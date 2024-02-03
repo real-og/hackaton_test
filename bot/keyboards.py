c42db2a3c6a9ccf2018cb110eeeb03ecfc1d3759
Evgeniy Grakovich <grakovich-e@yandex.by>
2024-02-03 11:49:03 +0300

    chossing currency interface

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -7,3 +7,10 @@ choose_bank_kb = ReplyKeyboardMarkup([[buttons.national_bank_btn], [
                                     buttons.alfa_bank_btn, buttons.belarus_bank_btn]],
                                      resize_keyboard=True,
                                      one_time_keyboard=True)
+
+choose_currency_kb = ReplyKeyboardMarkup([[buttons.usd_btn, 
+                                       buttons.eur_btn, 
+                                       buttons.gbp_btn, 
+                                       buttons.jpy_btn]],
+                                     resize_keyboard=True,
+                                     one_time_keyboard=True)
