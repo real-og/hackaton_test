b45940c711adb2a658e3d0deaccf36694a02616d
alexs <alexei.shef@gmail.com>
2024-02-03 12:54:13 +0300

    2 endpoint(currencies list from banks api)

---
+++ requests_to_api/banks.py
@@ -0,0 +1,5 @@
+# Api urls for bank currencies list
+currencies_list = {'nbrb': 'https://api.nbrb.by/exrates/currencies',
+                   'alfabank': 'https://developerhub.alfabank.by:8273/partner/1.0.1/public/rates',
+                   'belarusbank': 'https://belarusbank.by/api/kurs_cards'
+                   }
