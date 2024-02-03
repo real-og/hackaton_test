9333ed979cf928a101da14011076c518809f1d44
alexs <alexei.shef@gmail.com>
2024-02-03 12:54:13 +0300

    2 endpoint(currencies list from banks api)

---
+++ requests_to_api/bank_currencies_list.py
@@ -0,0 +1,11 @@
+import banks
+import requests
+
+url = banks.currencies_list['nbrb']  # 'nbrb', 'alfabank', 'belarusbank'
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error {response.status_code}: {response.text}")
